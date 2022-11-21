ee892304e24166542a8454335819dc248ee3a6d7
brick-ninja <davidliang0424@gmail.com>
2022-11-21 21:45:35 +0800

    add bottom navigator

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {BottomTabNavigator} from './screens/BottomTabs.navigator';
+
+export const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'teal',
+  },
+});
