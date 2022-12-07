12ddfc817c0ef0c9e2fdc81705d4b6fdbcf21229
brick-ninja <davidliang0424@gmail.com>
2022-12-07 11:54:49 +0800

    add using animate for the retangle

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Image, ImageBackground } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Pressable,
+} from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
-import { HomeIcon } from '../components/Icons';
-import { useSharedValue } from 'react-native-reanimated';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+const ReanimatedPressable = Reanimated.createAnimatedComponent(Pressable);
 
 const imageUrl =
   'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
@@ -13,10 +24,22 @@ export const Home: React.FC = () => {
 
   const shared = useSharedValue(0);
 
+  const style = useAnimatedStyle(
+    () => ({
+      transform: [{ translateX: shared.value }],
+    }),
+    [],
+  );
+
   return (
     <ImageBackground style={styles.container} source={{ uri: imageUrl }}>
       <MoodPicker handleSelectMood={appContext.handleSelectMood} />
-      <View style={styles.square} />
+      <ReanimatedPressable
+        onPress={() => {
+          shared.value = withTiming(shared.value + 20, { duration: 1000 });
+        }}
+        style={[styles.square, style]}
+      />
     </ImageBackground>
   );
 };
