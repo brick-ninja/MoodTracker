57b22954fe9f7daaf927d057fd84579ef806edef
brick-ninja <davidliang0424@gmail.com>
2022-12-08 00:10:30 +0800

    add animation for the button

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -3,6 +3,13 @@ import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
 import { MoodOptionType } from '../types';
 import { theme } from '../theme';
 
+import Reanimated, {
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+
+const ReanimatedPressable = Reanimated.createAnimatedComponent(Pressable);
+
 const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
@@ -20,6 +27,14 @@ const imageSrc = require('../../assets/butterflies.png');
 export const MoodPicker: React.FC<MoodPickerProps> = ({ handleSelectMood }) => {
   const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
   const [hasSelected, setHasSelected] = React.useState(false);
+
+  const buttonStyle = useAnimatedStyle(
+    () => ({
+      opacity: selectedMood ? withTiming(1) : withTiming(0.5),
+      transform: [{ scale: selectedMood ? withTiming(1) : withTiming(0.8) }],
+    }),
+    [selectedMood],
+  );
 
   const handleSelect = React.useCallback(() => {
     if (selectedMood) {
@@ -53,8 +68,7 @@ export const MoodPicker: React.FC<MoodPickerProps> = ({ handleSelectMood }) => {
                 option.emoji === selectedMood?.emoji
                   ? styles.selectedMoodItem
                   : undefined,
-              ]}
-            >
+              ]}>
               <Text style={styles.moodText}>{option.emoji}</Text>
             </Pressable>
             <Text style={styles.descriptionText}>
@@ -63,9 +77,11 @@ export const MoodPicker: React.FC<MoodPickerProps> = ({ handleSelectMood }) => {
           </View>
         ))}
       </View>
-      <Pressable style={styles.button} onPress={handleSelect}>
+      <ReanimatedPressable
+        style={[styles.button, buttonStyle]}
+        onPress={handleSelect}>
         <Text style={styles.buttonText}>Choose</Text>
-      </Pressable>
+      </ReanimatedPressable>
     </View>
   );
 };
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Image,
-  ImageBackground,
-  Pressable,
-} from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
-import Reanimated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-
-const ReanimatedPressable = Reanimated.createAnimatedComponent(Pressable);
 
 const imageUrl =
   'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
@@ -22,24 +9,9 @@ const imageUrl =
 export const Home: React.FC = () => {
   const appContext = useAppContext();
 
-  const shared = useSharedValue(0);
-
-  const style = useAnimatedStyle(
-    () => ({
-      transform: [{ translateX: shared.value }],
-    }),
-    [],
-  );
-
   return (
     <ImageBackground style={styles.container} source={{ uri: imageUrl }}>
       <MoodPicker handleSelectMood={appContext.handleSelectMood} />
-      <ReanimatedPressable
-        onPress={() => {
-          shared.value = withTiming(shared.value + 20, { duration: 1000 });
-        }}
-        style={[styles.square, style]}
-      />
     </ImageBackground>
   );
 };
